b974e6d4eca963fbc7f316fe85658d50ac65f660
HallogenicX <HallogenicX@localhost>
2020-06-25 15:28:52 -0400

    Emmanuel Girin 6/23/2020 Added all of the classes and some of the methods for the entire project. I updated all of the comments as well. We should decide if we want to keep the exception handling classes I originally devised.

---
+++ HotelReservationSystem/src/main/java/com/mycompany/hotelreservationsystem/ReservationManager.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hotelreservationsystem;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Emmanuel Girin
+ * Project: Hotel Reservation System
+ * Date: 06/23/2020
+ * Description: Receives data from GUI interface
+ *              Creates new reservations & modify existing reservations
+ *              Update and Query Database Reservation Records
+ */
+class ReservationManager {
+    //data
+    Reservation currentReservation;
+    
+    
+    //methods
+    
+    /**
+     * Create a new Reservation Object with data
+     * Set the current Reservation to this new object
+     * @param checkIn
+     * @param checkOut
+     * @param numGuests
+     * @param roomIDs 
+     */
+    void createReservation(Date checkIn, Date checkOut, int numGuests, int[] roomIDs){
+        
+    }
+    
+    /**
+     * Updates the database Reservation records 
+     * with the finalized reservation details
+     */
+    void finalizeReservation(){
+        
+    }
+    
+    /**
+     * Query database for reservation delete row if found else
+     * Throw an exception if database cannot find the current reservation
+     * @throws databaseLookUpException 
+     */
+    void deleteReservation() throws databaseLookUpException {
+        
+    }
+    
+    
+    /**
+     * Look up existing reservation in database 
+     * create new reservation object if found 
+     * @param customerID
+     * @param checkInDate 
+     */
+    void lookUpByCustomer(int customerID, Date checkInDate){
+        
+    }
+    
+    /**
+     * Look up existing reservation in database 
+     * create new reservation object if found 
+     * @param reservationNumber 
+     */
+    void lookUpByID (int reservationNumber) {
+        
+    }
+    
+    /**
+     * 
+     * @return currentReservation object
+     */
+    public Reservation getCurrentReservation() {
+        return currentReservation;
+    }
+    
+    
+    
+}

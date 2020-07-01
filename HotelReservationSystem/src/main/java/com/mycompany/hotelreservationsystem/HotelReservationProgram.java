eb3b22abf0916b037815a7fda952cf0ebb88eeaa
JacobValent25 <JacobValent25@gmail.com>
2020-07-01 06:04:02 +0300

    Update HotelReservationProgram.java
    Adds additional Swing elements to begin fleshing out the GUI and the different management panels.  Will require additional work and fine tuning in Sprint 2 to be complete.

--- HotelReservationSystem/src/main/java/com/mycompany/hotelreservationsystem/HotelReservationProgram.java
+++ HotelReservationSystem/src/main/java/com/mycompany/hotelreservationsystem/HotelReservationProgram.java
@@ -5,15 +5,23 @@
  */
 package com.mycompany.hotelreservationsystem;
 
-import java.awt.Frame;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Date;
 import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 /**
  *
  * @author Emmanuel Girin created overall structure
  * @date: 6/25/2020
+ * 
+ * @author Jacob Valentine added swing variables to begin fleshing out
+ * the GUI panels.
+ * @date: 6/30/2020
  * 
  * This class  calls the main function for the program,
  * provides the GUI interface along with the input output for a hotel
@@ -23,20 +31,60 @@ import javax.swing.JPanel;
 
 /**
  * Created by Emmanuel Girin 6/25 - basic structure added methods, some data values
+ * Modified by Jacob Valentine 6/20 - Added additional Swing variables, made 
+ * HotelReservationProgram an extension of JFrame, and implemented an actionListenr
  */
 
-public class HotelReservationProgram {
+public class HotelReservationProgram extends JFrame implements ActionListener {
     //GUI data members
     Frame frame;
     JPanel mainPanel;
     JPanel customerFormPanel;
     JPanel reservationFormPanel;
     
+    private final JLabel activeEmployeeIDLabel = new JLabel("Employee ID: ");
+    private final JLabel userTypeLabel = new JLabel("Current User Type: ");
+    private final JLabel activeCustomerLabel = new JLabel("Current Customer: ");
+    
+    
+    
+    
+    //Login Screen GUI Data members
+    private final int LOGIN_X = 500; //The default width of the Login window
+    private final int LOGIN_Y = 300; //The default hieght of the Login window
+    private final JLabel loginScreenLabel = new JLabel("Login");
+    private final JLabel employeeIDLabel = new JLabel("Employee ID: ");
+    private final JTextField employeeIDField = new JTextField();
+    private final JLabel passwordLabel = new JLabel("Password: ");
+    private final JTextField passwordField = new JTextField();
+    private final JButton loginButton = new JButton("Login");
+    
+    //CustomerFormPanel GUI Data members
+    private final int CUSTOMER_X = 400; //The default width of the CustomerManger window
+    private final int CUSTOMER_Y = 600; //The default hieght of the CustomerManager window
+    private final JButton logoutCustomerMangerButton = new JButton("Logout");
+    private final JLabel customerNameLabel = new JLabel("Customer Full Name: ");
+    private final JTextField customerNameField = new JTextField();
+    private final JLabel customerPhoneLabel = new JLabel("Customer Phone Number: ");
+    private final JTextField customerPhoneField = new JTextField();
+    private final JButton lookupCustomerButton = new JButton("Lookup Customer");
+    private final JButton updateCustomerButton = new JButton("Update Customer Account");
+    private final JButton createCustomerButton = new JButton("Create New Customer Account");  
+    
+    //ReservationFormPanel GUI Data Members
+    private final int RESERVATION_X = 400; //The default width of the CustomerManger window
+    private final int RESERVATION_Y = 600; //The default hieght of the CustomerManager window
+    private final JLabel reservationIDLabel = new JLabel("Reservation ID: ");
+    private final JTextField reservationIDField = new JTextField();
+    private final JButton selectReservationButton = new JButton("Select Reservation");
+    private final JLabel checkinDateLabel = new JLabel("Checkin Date: ");
+    private final JTextField checkinDateField = new JTextField();
+    
     //Non Gui Data members
-    ReservationManager reservMgr;
-    CustomerManager custMgr;
-    RoomManager roomMgr;
-    Login logon;
+    //ReservationManager reservMgr;
+    //CustomerManager custMgr;
+    //RoomManager roomMgr;
+    //Login logon;
     int  LogonAttemps = 0;
 
     /**
@@ -45,8 +93,8 @@ public class HotelReservationProgram {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        HotelReservationProgram a = new HotelReservationProgram();
-        a.connectDatabase();
+        //HotelReservationProgram a = new HotelReservationProgram();
+        //a.connectDatabase();
     }
     
     
@@ -70,7 +118,7 @@ public class HotelReservationProgram {
         int userID = -1;
         String password = "";
       
-        logon.validateCredentials(userID, password);
+        //logon.validateCredentials(userID, password);
         
     }
     
@@ -82,10 +130,10 @@ public class HotelReservationProgram {
      */
     void displayCustomerManager(){
         //GUI datamembers
-        JButton newCustomer, existingCustomer;
+        //JButton newCustomer, existingCustomer;
         
         //initialize CustomerManager
-        custMgr = new CustomerManager();
+        //custMgr = new CustomerManager();
     }
     
     /**
@@ -123,7 +171,7 @@ public class HotelReservationProgram {
      */
     void displayReservationManager(){
         //GUI data
-        JButton newReservation, existingReservation;
+        //JButton newReservation, existingReservation;
         
         //Non-GUI data
         
@@ -166,11 +214,11 @@ public class HotelReservationProgram {
          //GUI data
        
         //Non-GUI data       
-        Date checkIn;
-        Date checkOut;
-        int numOfGUests;
-        int roomType;
-        int numOfRoomsNeeded;
+        //Date checkIn;
+        //Date checkOut;
+        //int numOfGUests;
+        //int roomType;
+        //int numOfRoomsNeeded;
         
         
     }
@@ -187,6 +235,99 @@ public class HotelReservationProgram {
         //Non-GUI data       
     }
     
+    /**
+     * Created by Jacob Valentine 6/30
+     * Registers basic actions done to the GUI, such as clicking a button or
+     * typing in information into a text field, and triggers relevant methods.
+     * 
+     * @param e 
+     */
+    public void actionPerformed(ActionEvent e){
+        
+    }
+    
+    /**
+     * Created by Jacob Valentine 6/30
+     * initializes all the panels used for the different management screens.
+     */
+    public void initializePanels(){
+        //Initialize the login screen panel.
+        mainPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridwidth = 2;
+        c.gridx = 0;
+        c.gridy = 0;
+        mainPanel.add(loginScreenLabel, c);
+        c.gridwidth = 1;
+        c.gridx = 0;
+        c.gridy = 1;
+        mainPanel.add(employeeIDLabel, c);
+        c.gridx = 1;
+        c.gridy = 1;
+        mainPanel.add(employeeIDField, c);
+        c.gridx = 0;
+        c.gridy = 2;
+        mainPanel.add(passwordLabel, c);
+        c.gridx = 1;
+        c.gridy = 2;
+        mainPanel.add(passwordField, c);
+        c.gridx = 0;
+        c.gridy = 3;
+        c.gridwidth = 2;
+        mainPanel.add(loginButton, c);
+        employeeIDField.setEditable(true);
+        passwordField.setEditable(true);
+        loginButton.addActionListener(this);
+        
+        //Initialize the customer manager screen panel.
+        customerFormPanel = new JPanel(new GridBagLayout());
+        c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridwidth = 2;
+        c.gridx = 0;
+        c.gridy = 0;
+        customerFormPanel.add(activeEmployeeIDLabel, c);
+        c.gridx = 0;
+        c.gridy = 1;
+        customerFormPanel.add(userTypeLabel, c);
+        c.gridx = 0;
+        c.gridy = 2;
+        customerFormPanel.add(logoutCustomerMangerButton, c);
+        c.gridwidth = 1;
+        c.gridx = 0;
+        c.gridy = 3;
+        customerFormPanel.add(customerNameLabel, c);
+        c.gridx = 2;
+        c.gridy = 3;
+        customerFormPanel.add(customerNameField, c);
+        c.gridx = 0;
+        c.gridy = 4;
+        customerFormPanel.add(customerPhoneLabel, c);
+        c.gridx = 1;
+        c.gridy = 4;
+        customerFormPanel.add(customerPhoneField, c);
+        c.gridwidth = 2;
+        c.gridx = 0;
+        c.gridy = 5;
+        customerFormPanel.add(lookupCustomerButton, c);
+        c.gridx = 0;
+        c.gridy = 6;
+        customerFormPanel.add(updateCustomerButton, c);
+        c.gridx = 0;
+        c.gridy = 7;
+        customerFormPanel.add(createCustomerButton, c);
+        lookupCustomerButton.addActionListener(this);
+        updateCustomerButton.addActionListener(this);
+        createCustomerButton.addActionListener(this);
+        
+        //Initialize the reservation manager screen panel
+        //TODO
+    }
     
     
 }

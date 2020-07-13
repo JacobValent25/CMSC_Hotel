e9386adf690d2edb59666b9dacf7daf9fb32223b
EmmanuelGirin <66262049+EmmanuelGirin@users.noreply.github.com>
2020-07-12 23:21:40 -0400

    Update TableData.sql
    Updated Table Data

--- TableData.sql
+++ TableData.sql
@@ -9,7 +9,6 @@ CREATE USER IF NOT EXISTS 'hotelemployee'@'localhost' IDENTIFIED BY 'password';
 GRANT SELECT, INSERT, DELETE ON hotelManager.* TO hotelemployee@'localhost';
 FLUSH PRIVILEGES;
 
-
 -- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
 --
 -- Host: localhost    Database: hotelmanager
@@ -44,20 +43,11 @@ CREATE TABLE `customerrecords` (
   `country` varchar(50) DEFAULT NULL,
   `phone` varchar(50) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
+  `DOB` date DEFAULT NULL,
   PRIMARY KEY (`customerID`),
   UNIQUE KEY `customerID_UNIQUE` (`customerID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `customerrecords`
---
-
-LOCK TABLES `customerrecords` WRITE;
-/*!40000 ALTER TABLE `customerrecords` DISABLE KEYS */;
-INSERT INTO `customerrecords` VALUES (1,'leleo','Joins','street','city','MD','USA','4477558784','test@umuc.edu');
-/*!40000 ALTER TABLE `customerrecords` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `reservationrecords`
@@ -79,18 +69,8 @@ CREATE TABLE `reservationrecords` (
   UNIQUE KEY `reservationID_UNIQUE` (`reservationID`),
   KEY `res_customerID` (`customerID`),
   CONSTRAINT `res_customerID` FOREIGN KEY (`customerID`) REFERENCES `customerrecords` (`customerID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `reservationrecords`
---
-
-LOCK TABLES `reservationrecords` WRITE;
-/*!40000 ALTER TABLE `reservationrecords` DISABLE KEYS */;
-INSERT INTO `reservationrecords` VALUES (1,1,'1, 2','2017-06-15','2017-06-17',0,4,2),(2,1,'2, 1','2020-07-30','2020-08-05',200.5,2,2);
-/*!40000 ALTER TABLE `reservationrecords` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `roombookings`
@@ -115,16 +95,6 @@ CREATE TABLE `roombookings` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `roombookings`
---
-
-LOCK TABLES `roombookings` WRITE;
-/*!40000 ALTER TABLE `roombookings` DISABLE KEYS */;
-INSERT INTO `roombookings` VALUES (1,1,1,'2017-06-15','2017-06-17'),(2,2,1,'2017-06-15','2017-06-17'),(4,2,1,'2020-07-30','2020-08-05');
-/*!40000 ALTER TABLE `roombookings` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `roomrecords`
 --
 
@@ -143,16 +113,6 @@ CREATE TABLE `roomrecords` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `roomrecords`
---
-
-LOCK TABLES `roomrecords` WRITE;
-/*!40000 ALTER TABLE `roomrecords` DISABLE KEYS */;
-INSERT INTO `roomrecords` VALUES (1,204,'testHotel','Single',24.5),(2,205,'testHotel','Suite',74.5);
-/*!40000 ALTER TABLE `roomrecords` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `userrecords`
 --
 
@@ -168,16 +128,6 @@ CREATE TABLE `userrecords` (
   UNIQUE KEY `empID_UNIQUE` (`empID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `userrecords`
---
-
-LOCK TABLES `userrecords` WRITE;
-/*!40000 ALTER TABLE `userrecords` DISABLE KEYS */;
-INSERT INTO `userrecords` VALUES (140111,'1455444','John','Candy');
-/*!40000 ALTER TABLE `userrecords` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -188,4 +138,81 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-07-11 11:50:44
+-- OPTIONAL Add Data for Testing
+-- USER RECORDS
+INSERT INTO `userRecords` VALUES (140111,'1455444','John','Candy');
+
+-- Customer Records
+INSERT INTO `customerRecords` VALUES 
+    (1,'leleo','Joins','street','city','MD','USA','4477558784','test@umuc.edu','1999-01-01' ),
+    (2,'Val','Junior','street','city','MD','USA','44455577789','test2@umuc.edu','1992-04-01'),
+    (3,'Caio','Gran','street','city','MD','USA','7778889994','test3@umuc.edu','1955-01-05'),
+    (4,'Ronald','MT','street','city','MD','USA','7774441112','test3@umuc.edu', '1999-02-01'),
+    (5,'Indi','Ora','street','city','MD','USA','11122233364','test3@umuc.edu', '1999-01-02'),
+    (6,'Pedro','Span','street','city','MD','USA','44477252123','test3@umuc.edu', '1999-01-30'),
+    (7,'Marcos','BR','street','city','MD','USA','78999544565','test3@umuc.edu', '1985-01-01');
+
+-- Room Records 
+INSERT INTO `roomRecords` VALUES 
+    (1,101,'CMSC HOTEL','Single',24.5),
+    (2,102,'CMSC HOTEL','Single',24.5),
+    (3,103,'CMSC HOTEL','Single',24.5),
+    (4,104,'CMSC HOTEL','Single',24.5),
+    (5,105,'CMSC HOTEL','Suite',74.5),
+    (6,106,'CMSC HOTEL','Suite',74.5),
+    (7,107,'CMSC HOTEL','Suite',74.5),
+    (8,108,'CMSC HOTEL','Suite',74.5),
+    (9,201,'CMSC HOTEL','Single',24.5),
+    (10,202,'CMSC HOTEL','Single',24.5),
+    (11,203,'CMSC HOTEL','Single',24.5),
+    (12,204,'CMSC HOTEL','Single',24.5),
+    (13,205,'CMSC HOTEL','Suite',74.5),
+    (14,206,'CMSC HOTEL','Suite',74.5),
+    (15,207,'CMSC HOTEL','Suite',74.5),
+    (16,208,'CMSC HOTEL','Suite',74.5),
+    (17,301,'CMSC HOTEL','Single',24.5),
+    (18,302,'CMSC HOTEL','Single',24.5),
+    (19,303,'CMSC HOTEL','Single',24.5),
+    (20,304,'CMSC HOTEL','Single',24.5),
+    (21,305,'CMSC HOTEL','Suite',74.5),
+    (22,306,'CMSC HOTEL','Suite',74.5),
+    (23,307,'CMSC HOTEL','Suite',74.5),
+    (24,308,'CMSC HOTEL','Suite',74.5),
+    (25,401,'CMSC HOTEL','Single',24.5),
+    (26,402,'CMSC HOTEL','Single',24.5),
+    (27,403,'CMSC HOTEL','Single',24.5),
+    (28,404,'CMSC HOTEL','Single',24.5),
+    (29,405,'CMSC HOTEL','Suite',74.5),
+    (30,406,'CMSC HOTEL','Suite',74.5),
+    (31,407,'CMSC HOTEL','Suite',74.5),
+    (32,408,'CMSC HOTEL','Suite',74.5);
+
+
+-- Reservation Records
+INSERT INTO `reservationrecords` VALUES 
+    (1,2,'1, 8','2019-11-15','2019-11-17',0,4,2),
+    (2,4,'2, 9','2019-11-15','2019-11-17',0,4,2),
+    (3,6,'3, 10','2019-11-15','2019-11-17',0,4,2),
+    (4,7,'4, 11','2019-11-15','2019-11-17',0,4,2),
+    (5,1,'5','2020-04-15','2020-04-17',0,4,2),
+    (6,3,'6','2020-04-15','2020-04-17',0,4,2),
+    (7,5,'7','2020-04-15','2020-04-17',0,4,2);
+
+-- Room Bookings
+INSERT INTO `roomBookings` VALUES 
+  (1,1,1,'2019-11-15','2019-11-17'),
+  (2,10,2,'2019-11-15','2019-11-17'),
+  (3,7,3,'2019-11-15','2019-11-17'),
+  (4,8,4,'2019-11-15','2019-11-17'),
+  (5,6,5,'2020-04-15','2020-04-17'),
+  (6,20,6,'2020-04-15','2020-04-17'),
+  (7,3,7,'2020-04-15','2020-04-17');
+
+-- SELECT TEST
+select * from userRecords;
+select * from customerRecords;
+select * from roomRecords;
+select * from reservationrecords;
+select * from roomBookings;
+
+-- Dump completed on 2020-07-12 23:18:30

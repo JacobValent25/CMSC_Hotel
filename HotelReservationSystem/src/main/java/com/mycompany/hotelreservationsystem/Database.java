5730e630b0bd6e396840574fbb0ae31c7871a01e
Emmanuel <Emmanuel@Hallogenic-X>
2020-07-13 13:37:30 -0400

    7/11 - EG: Removed JFrame from and all of its references from main class 	   Tested Logon class successfully connects to database 	   Tested Error handling, and implemented locking user out after 5        bad password attempts
    7/12 - GUI mostly redone working on functionality of logic now, fixed most errors still one SQL query not working
    
    7/13 Finalized

--- HotelReservationSystem/src/main/java/com/mycompany/hotelreservationsystem/Database.java
+++ HotelReservationSystem/src/main/java/com/mycompany/hotelreservationsystem/Database.java
@@ -1,6 +1,7 @@
 package com.mycompany.hotelreservationsystem;
 import java.sql.*;
 
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
